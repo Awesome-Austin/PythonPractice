a0b0a1cf3631ef9d92a4d535c2834f9b519c7193
Awesome_Austin <austin.gifford@gmail.com>
2020-08-06 16:45:55 -0700

    Created new package ReadMeGrabber. Moved and renamed DailyProgrammer Grabber to RedditGrabber
    ;

--- ReadMeGrabber/RedditGrabber.py
+++ ReadMeGrabber/RedditGrabber.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def create_readme(url):
+def create_reddit_readme(url):
     res = requests.get(url)
     try:
         res.raise_for_status()
---
+++ ReadMeGrabber/__init__.py
@@ -0,0 +1,1 @@
+from ReadMeGrabber import RedditGrabber
