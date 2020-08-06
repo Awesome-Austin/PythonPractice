bb1751afc866399313b40a994c2ea0c30aec152c
Awesome_Austin <austin.gifford@gmail.com>
2020-08-06 16:43:43 -0700

    added Daily Programmer Grabber

---
+++ r_DailyProgrammer/DailyProgrammerGrabber.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def create_readme(url):
+    res = requests.get(url)
+    try:
+        res.raise_for_status()
+    except Exception as e:
+        raise e
+
+    print(res.text)
+
+
+if __name__ == '__main__':
+    url = r"https://www.reddit.com/r/dailyprogrammer/comments/hrujc5/20200715_challenge_385_intermediate_the_almost/"
+    create_readme(url)
